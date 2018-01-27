c1c61479c6aa5134244a8b7281700ecf99ccf289
amrangaye <amrangaye@gmail.com>
2018-01-27 21:18:36 +0000

    add description field to category

--- api/barrowmeter/models/PromiseCategory.js
+++ api/barrowmeter/models/PromiseCategory.js
@@ -11,6 +11,7 @@ var PromiseCategory = new keystone.List('PromiseCategory', {
 
 PromiseCategory.add({
 	name: { type: String, required: true },
+	description: {type: String}
 });
 
 PromiseCategory.relationship({ ref: 'Promise', path: 'promises', refPath: 'categories' });
