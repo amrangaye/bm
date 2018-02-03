e7898a5b4672630560ce351ee81cf777614a7511
amrangaye <amrangaye@gmail.com>
2018-02-03 10:05:30 +0000

    react native elements working

--- app/barrowmeter/App.js
+++ app/barrowmeter/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+// import { Container, Header, Content, Card, CardItem, Body, Text } from 'native-base';
+import { Button } from 'react-native-elements';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <Button
+  title='BUTTON' />
+
+<Button
+  raised
+  icon={{name: 'cached'}}
+  title='BUTTON WITH ICON' />
+
+<Button
+  large
+  iconRight={{name: 'code'}}
+  title='LARGE WITH RIGHT ICON' />
+
+<Button
+  large
+  icon={{name: 'envira', type: 'font-awesome'}}
+  title='LARGE WITH ICON TYPE' />
+
+<Button
+  large
+  icon={{name: 'squirrel', type: 'octicon', buttonStyle: styles.someButtonStyle }}
+  title='OCTICON' />
       </View>
     );
   }
